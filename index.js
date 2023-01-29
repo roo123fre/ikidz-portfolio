09de5b89360fcfb4907e87638ac6aa3a0d6bc0cb
roo123fre <kibichdaneil@gmail.com>
2023-01-29 14:45:40 +0200

    added  burger button animation

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const menu = document.querySelector('.menu')
 const menuItem = document.querySelector('.header__nav-list')
-
-
+const portfolio = document.querySelector('.portfolio__information')
+const buttonActive = document.querySelector('.button__active')
 
 menu.addEventListener('click',()=>{
-    menuItem.classList.toggle('active')
+    menuItem.classList.toggle('active');
+})
+buttonActive.addEventListener('click',()=>{
+    menu.classList.toggle('button__active');
+})
+
+window.addEventListener('scroll', ()=>{
+    console.log('scrolled!');
 })
 
